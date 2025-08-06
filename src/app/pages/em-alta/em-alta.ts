3dc5108bf3e6cc04a62a5212ac3d0f69fd50fa6d
Celline Bitencourt <149202629+cellineb@users.noreply.github.com>
2025-08-06 16:30:47 -0300

    add navbar pages; add backgroung videos (#25)

--- src/app/pages/em-alta/em-alta.ts
+++ src/app/pages/em-alta/em-alta.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
+import { PackageService } from '../../services/packageService';
+import { PackageCard } from '../../components/package-card/package-card';
 
 @Component({
   selector: 'app-em-alta',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, PackageCard],
   templateUrl: './em-alta.html',
   styleUrl: './em-alta.css'
 })
 export class EmAlta {
+  item: any;
 
+  constructor(private packageService: PackageService) { }
+
+  ngOnInit() {
+    this.packageService.getAllPackages().subscribe({
+      next: (res) => {
+        this.item = res;
+        console.log(`All Packages:`, this.item);
+      },
+      error: (err) => console.error(err)
+    });
+  }
 }
