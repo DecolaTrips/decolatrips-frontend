33b7cca125d3d222722d5062cacc2c44b3210e75
Denise Korgiski de Oliveira <dkorgiski@gmail.com>
2025-08-06 01:42:07 -0300

    Feat: implement reset password functionality with form validation and API integration

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,29 +2,32 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { User, RegisterUser } from '../models/user.model';
 import { HttpClient } from '@angular/common/http';
-import { AuthResponse } from '../interfaces/auth-response';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private route = "http://localhost:8080/api/auth"
 
   constructor(private http: HttpClient) { }
-  baseUrl: string = "http://localhost:8080/api/auth"
 
-  login(user: User): Observable<AuthResponse> {
-      const rota: string = "login";
-      return this.http.post<AuthResponse>(`${this.baseUrl}/${rota}`, user);
-    }
-
-  register(user: RegisterUser): Observable<any> {
-    const rota: string = "register";
-    return this.http.post<any>(`${this.baseUrl}/${rota}`, user);
+  login(user: User): Observable<any> {
+        return this.http.post(`${this.route}/login`, user);
   }
 
-  forgotPassword(email: string): Observable<boolean> {
+  register(user: RegisterUser): Observable<boolean> {
     // simulação chamada de API
-    console.log('Forgot password for:', email);
+    console.log('Register attempt:', user);
     return of(true);
   }
+
+  forgotPassword(email: string): Observable<any> {
+    return this.http.post(`${this.route}/password/forgot`, null, { params: { email: email } })
+  }
+
+  resetPassword(token: string, newPassword: string, confirmPassword: string): Observable<any> {
+    return this.http.post(`${this.route}/password/reset?token=${token}`, { newPassword, confirmPassword })
+  }
+
 }
