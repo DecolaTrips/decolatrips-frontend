7088b5c7f3327262f2640009d39e23ec4fd24c21
cellineb <cellinebitencourt@gmail.com>
2025-08-06 16:26:02 -0300

    add navbar pages; add backgroung videos

--- src/app/pages/destinos-internacionais/destinos-internacionais.ts
+++ src/app/pages/destinos-internacionais/destinos-internacionais.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
+import { PackageCard } from '../../components/package-card/package-card';
 
 @Component({
   selector: 'app-destinos-internacionais',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, PackageCard],
   templateUrl: './destinos-internacionais.html',
   styleUrl: './destinos-internacionais.css'
 })
--- src/app/pages/destinos-nacionais/destinos-nacionais.ts
+++ src/app/pages/destinos-nacionais/destinos-nacionais.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
+import { PackageCard } from '../../components/package-card/package-card';
 
 @Component({
   selector: 'app-destinos-nacionais',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, PackageCard],
   templateUrl: './destinos-nacionais.html',
   styleUrl: './destinos-nacionais.css'
 })
