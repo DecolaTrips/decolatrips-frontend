d4fd920e1f7ef7fbbcf05ddc7fd1183519a6e36d
d.antonio.dos.santos@avanade.com <daviantoniosantos@outlook.com>
2025-08-07 15:51:21 -0300

    chore: clean up unused files and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([]),
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'disabled',
+    }),
     AppComponent
   ],
   providers: [],
