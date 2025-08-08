83432ba17d61c3ba2baba51f4b9177edecbdeb83
d.antonio.dos.santos@avanade.com <daviantoniosantos@outlook.com>
2025-08-08 01:12:14 -0300

    feat: implement rating service and integrate ratings in the rating component

--- src/app/components/rating/rating.ts
+++ src/app/components/rating/rating.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { RatingCommentCard } from '../rating-comment-card/rating-comment-card';
+import { RatingService } from '../../services/rating.service';
 
 @Component({
   selector: 'app-rating',
@@ -9,9 +10,32 @@ import { RatingCommentCard } from '../rating-comment-card/rating-comment-card';
 })
 export class Rating {
   maxRating: number = 5;
+  comments: Array<any> = [];
+  rating: number = 0;
 
-  @Input() rating: number = 0;
-  @Input() comments: Array<{ name: string; rating: number; comment: string }> = [];
+  constructor(private ratingService: RatingService) {}
+
+  @Input() packageId: number = 0;
+
+  ngOnInit() {
+    console.log('Package ID:', this.packageId);
+    this.ratingService.getRatings(this.packageId).subscribe({
+      next: (data) => {
+        this.comments = data;
+
+        if (this.comments.length > 0) {
+          const somaRatings = this.comments.reduce((total, item) => total + item.rating, 0);
+          this.rating = somaRatings / this.comments.length;
+        } else {
+          this.rating = 0; // caso não haja avaliações
+        }
+
+      },
+      error: (err) => {
+        console.error('Error fetching ratings:', err);
+      }
+    });
+  }
 
   get fullStars(): any[] {
     return Array(Math.ceil(this.rating));
